8d363d13f07325e19e18f9da73f09f7a21eb7b82
1312167352 <1312167352@qq.com>
2019-12-14 09:24:18 +0800

    三次上传

---
+++ runtime/temp/6d9ea501b6e4d90e8d3d9501fbe40401.php
@@ -0,0 +1,25 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:2:{s:63:"D:\www\hao365\public/../application/index\view\index\index.html";i:1576225604;s:42:"../application/index/view/common/head.html";i:1576223166;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/static/style/home/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="/static/style/home/css/css.css"/>
+</head>
+<body>
+    <div class="container">
+        <div class="head clearfix">
+    <div class="col-md-12 welcome">欢迎光临 <span class='pull-right'>收藏网站 | 添加首页</span></div>
+    <div class="tianqi">
+        <img src="/static/style/home/img/logo.png" alt="" class='pull-left'>
+        <iframe name="weather_inc" src="http://i.tianqi.com/index.php?c=code&id=2&num=5" width="650" height="70" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
+    </div>
+</div>
+        <div class="main"></div>
+        <div class="foot"></div>
+    </div>
+</body>
+</html>
